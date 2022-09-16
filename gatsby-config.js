682007a2b5a92ff5c0ce339e9774ccf689452f20
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2022-09-17 05:10:26 +0900

    gatsby-plugin-manifestのnameを変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `XENEXE`,
+        short_name: `XENEXE`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
