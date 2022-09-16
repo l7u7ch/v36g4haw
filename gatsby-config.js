0ff63cc3ea98f7cbbd56c9edd17f2e8835d6b909
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2022-09-17 05:06:18 +0900

    gatsby-plugin-manifest と gatsby-plugin-offline を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,10 +15,23 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-twitter",
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
         siteUrl: `https://www.example.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
       },
     },
     {
