e621d9c7f74d328dd3263b9942480f90fee4e2aa
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2022-09-18 04:55:40 +0900

    ホーム画面のデザインを変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,25 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { Link, graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
 const IndexPage = ({ data: { allContentfulBlogPost } }) => {
   return (
     <Layout>
-      <div className="p-8">
-        <ul className="list-inside list-disc">
-          {allContentfulBlogPost.nodes.map((node) => (
-            <li>
-              <Link to={node.id}>{node.title}</Link>
-            </li>
-          ))}
-        </ul>
+      <div className="grid grid-cols-1 items-start gap-4 px-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {allContentfulBlogPost.nodes.map((node) => (
+          <Link className="rounded-lg bg-slate-800 duration-500 hover:scale-105" to={node.id}>
+            <GatsbyImage image={node.heroImage.gatsbyImageData} className={"rounded-t-lg"} />
+            <div className="p-4">
+              <div className="mb-2">{node.title}</div>
+              <div className="text-right">
+                <FontAwesomeIcon className="" icon={faRotate} fixedWidth /> {node.updatedAt}
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </Layout>
   );
@@ -27,10 +34,14 @@ export const Head = ({ data }) => {
 
 export const pageQuery = graphql`
   query {
-    allContentfulBlogPost {
+    allContentfulBlogPost(sort: { fields: updatedAt, order: DESC }) {
       nodes {
         id
         title
+        heroImage {
+          gatsbyImageData
+        }
+        updatedAt(formatString: "YYYY-MM-DD")
       }
     }
   }
