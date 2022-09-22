0a9f962aed4be893e0bc2d5acfeca6265405cd15
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2022-09-22 21:54:27 +0900

    TOC のデザインを変更

--- src/components/toc.js
+++ src/components/toc.js
@@ -4,24 +4,26 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const TocComponent = ({ props }) => {
   return (
-    <ul className="pl-4">
-      {props.map((item) => (
-        <>
-          {item.items ? (
-            <>
+    <div className="rounded-lg bg-slate-800 py-4 pr-4">
+      <ul className="pl-4">
+        {props.map((item) => (
+          <>
+            {item.items ? (
+              <>
+                <AnchorLink href={item.url}>
+                  <li className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</li>
+                </AnchorLink>
+                <Toc props={item.items} />
+              </>
+            ) : (
               <AnchorLink href={item.url}>
                 <li className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</li>
               </AnchorLink>
-              <Toc props={item.items} />
-            </>
-          ) : (
-            <AnchorLink href={item.url}>
-              <li className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</li>
-            </AnchorLink>
-          )}
-        </>
-      ))}
-    </ul>
+            )}
+          </>
+        ))}
+      </ul>
+    </div>
   );
 };
 
