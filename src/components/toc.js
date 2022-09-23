3bc971eb35c42a2f3915366d1c025905489146db
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2022-09-23 12:16:45 +0900

    ul・li を div に変更

--- src/components/toc.js
+++ src/components/toc.js
@@ -6,24 +6,24 @@ const TocComponent = ({ props }) => {
   return (
     <>
       {props ? (
-        <ul className="pl-4">
+        <div className="pl-4">
           {props.map((item) => (
             <>
               {item.items ? (
                 <>
                   <AnchorLink href={item.url}>
-                    <li className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</li>
+                    <div className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</div>
                   </AnchorLink>
                   <Toc props={item.items} />
                 </>
               ) : (
                 <AnchorLink href={item.url}>
-                  <li className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</li>
+                  <div className="rounded-lg px-4 py-2 duration-500 hover:bg-slate-700">{item.title}</div>
                 </AnchorLink>
               )}
             </>
           ))}
-        </ul>
+        </div>
       ) : (
         ""
       )}
