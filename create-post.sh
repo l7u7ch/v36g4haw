164affd691468c17a82f5a2a16c759df46c707ef
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-01-23 16:52:08 +0000

    記事の雛形を自動的に生成するスクリプトを追加

---
+++ create-post.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_datetime=$(date +"%Y-%m-%d %H:%M")
+folder_name=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 4 | tr '\n' '-' | sed 's/-$//')
+folder_path="./content/posts/${folder_name}"
+mkdir -p "$folder_path"
+cat << EOF > "${folder_path}/index.md"
+---
+title: UNTITLE
+createdAt: $current_datetime
+updatedAt: $current_datetime 
+heroImage: 
+---
+EOF
