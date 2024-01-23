3f4f241e2d4c37d89d7b7ca66f8b09badf05c1e2
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-01-23 20:09:14 +0000

    メタデータコンポーネントをリファクタリング

--- src/components/metadata.js
+++ src/components/metadata.js
@@ -6,32 +6,42 @@ const MetadataComponent = ({ createdAt = "1999-11-11", updatedAt = "1999-11-11",
   return (
     <div className="rounded-lg bg-slate-800">
       {/* 1. 投稿日 */}
-      <div className="flex justify-between border-slate-700 px-6 py-4">
-        <span className="flex items-center">
-          <FaCalendarDays /> &nbsp; 投稿日
+      <div className="flex justify-between px-6 py-4">
+        <span>
+          <FaCalendarDays className="inline" />
+          {" 投稿日"}
         </span>
         {dayjs(createdAt).format(`YYYY-MM-DD`)}
       </div>
+      <hr className="border-t-2 border-slate-700" />
       {/* 2. 更新日 */}
-      <div className="flex justify-between border-t-2 border-slate-700 px-6 py-4">
-        <span className="flex items-center">
-          <FaRotate /> &nbsp; 更新日
+      <div className="flex justify-between px-6 py-4">
+        <span>
+          <FaRotate className="inline" />
+          {" 更新日"}
         </span>
         {dayjs(updatedAt).format(`YYYY-MM-DD`)}
       </div>
+      <hr className="border-t-2 border-slate-700" />
       {/* 3. 文字数 */}
-      <div className="flex justify-between border-t-2 border-slate-700 px-6 py-4">
-        <span className="flex items-center">
-          <FaFileLines /> &nbsp; 文字数
+      <div className="flex justify-between px-6 py-4">
+        <span>
+          <FaFileLines className="inline" />
+          {" 文字数"}
         </span>
-        {words} 文字
+        {words}
+        {" 文字"}
       </div>
+      <hr className="border-t-2 border-slate-700" />
       {/* 4. 読了時間 */}
-      <div className="flex justify-between border-t-2 border-slate-700 px-6 py-4">
-        <span className="flex items-center">
-          <FaBookOpen /> &nbsp; 読了時間
+      <div className="flex justify-between px-6 py-4">
+        <span>
+          <FaBookOpen className="inline" />
+          {" 読了時間"}
         </span>
-        約 {Math.ceil(words / 400)} 分
+        {"約 "}
+        {Math.ceil(words / 400)}
+        {" 分"}
       </div>
     </div>
   );
