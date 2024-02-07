e8e36290bb7e4ba5db4a2ec4e60e86a8dbea7483
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-02-07 10:36:08 +0000

    プロフィールに Bluesky を追加

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@ export const SITE_DESCRIPTION = 'ここにサイトの説明を入れる'
 export const AUTHOR_NAME = 'l7u7ch'
 export const AUTHOR_BIO =
   'コンピュータサイエンスを専攻している大学院生です。研究の合間に，アニメを観たりブログサイトを運営しながら生きています。'
+export const BLUESKY__SERVER = '.bsky.social'
+export const BLUESKY_ID = 'l7u7ch'
 export const GITHUB_ID = 'l7u7ch'
 export const MISSKEY_SERVER = 'misskey.io'
 export const MISSKEY_ID = 'l7u7ch'
