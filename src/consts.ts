212e200f08df167e11cb81240aa95ce65b7e78b4
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-05-06 19:55:34 +0000

    サイトの説明を更新

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 export const SITE_TITLE = 'XENEXE'
-export const SITE_DESCRIPTION = 'ここにサイトの説明を入れる'
+export const SITE_DESCRIPTION =
+  '本サイトは、l7u7ch が管理・運用しているブログサイトです。主にコンピューターサイエンスやプロダクティビティに関する記事を投稿しています。'
 export const AUTHOR_NAME = 'l7u7ch'
 export const AUTHOR_BIO =
   'コンピュータサイエンスを専攻している大学院生です。研究の合間に，アニメを観たりブログサイトを運営しながら生きています。'
