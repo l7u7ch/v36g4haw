ecd30941b00f79dbb73262c3f5634bf2d891c80d
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-05-07 18:26:35 +0000

    定数を整理

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,6 @@
 export const SITE_TITLE = 'XENEXE'
-export const SITE_DESCRIPTION =
-  '本サイトは、l7u7ch が管理・運用しているブログサイトです。主にコンピューターサイエンスやプロダクティビティに関する記事を投稿しています。'
+export const SITE_DESCRIPTION = `
+  本サイトは、l7u7ch が管理・運用しているブログサイトです。
+  主にコンピューターサイエンスやプロダクティビティに関する記事を投稿しています。
+`
 export const AUTHOR_NAME = 'l7u7ch'
-export const AUTHOR_BIO =
-  'コンピュータサイエンスを専攻している大学院生です。研究の合間に，アニメを観たりブログサイトを運営しながら生きています。'
-export const BLUESKY__SERVER = '.bsky.social'
-export const BLUESKY_ID = 'l7u7ch'
-export const EMAIL_ADDRESS = 'public.iaic8@slmail.me'
-export const GITHUB_ID = 'l7u7ch'
